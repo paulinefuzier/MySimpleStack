3f7de585eae24d5c0afc061ac0ace0a7e9b3b171
paulinefuzier <paulinefuzier@hotmail.fr>
2017-10-26 14:10:44 +0200

    premier commit

---
+++ src/main/java/deqo/pfuz/App.java
@@ -0,0 +1,17 @@
+package deqo.pfuz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /**
+     *
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
